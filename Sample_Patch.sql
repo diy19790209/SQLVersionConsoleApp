9bcda8cb947e6602ddf71ec36d3d9fe20c909ead
jasonwang <diy19790209@hotmail.com>
2024-02-21 16:45:26 +0800

    Add files via upload

---
+++ Sample_Patch.sql
@@ -0,0 +1,26 @@
+###NEW_VERSION### Version=1
+###COMMENT###
+註解
+###SQL###
+
+要執行的SQL
+
+###END###
+
+###NEW_VERSION### Version=2.1
+###COMMENT###
+註解
+###SQL###
+
+要執行的SQL
+
+###END###
+
+###NEW_VERSION### Version=2.1.2
+###COMMENT###
+註解
+###SQL###
+
+要執行的SQL
+
+###END###
